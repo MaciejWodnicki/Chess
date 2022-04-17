1785f11c53485ff4a6dd46f832faf877edb07243
Maciej Wodnicki <wodnicki.maciej@gmail.com>
2022-04-17 20:59:17 +0200

    fully working queen movement

--- src/pieces/Queen.java
+++ src/pieces/Queen.java
@@ -24,10 +24,118 @@ public class Queen extends Piece{
             }
         }
 
-        if((y-m_y!=x-m_x && y-m_y!=m_x-x )&&(m_x!=x && m_y!=y))
+        if((y-m_y!=x-m_x && y-m_y!=m_x-x )&&(m_x!=x && m_y!=y)) // is it on diagonals or vertical/horizontal
         {
             return false;
         }
+
+        for(int i=-1; i<=1;i++) //king-like move check
+        {
+            for(int j=-1; j<=1;j++)
+            {
+                if(x==m_x+i && y==m_y+j)
+                    return true;
+            }
+
+        }
+
+
+        int dy;
+        int dx;
+
+        if(m_y < y){
+            dy = 1;
+        }else if (m_y == y){
+            dy = 0;
+        }else{
+            dy = -1;
+        }
+
+        if(m_x < x){
+            dx = 1;
+        }else if(m_x==x){
+            dx = 0;
+        }
+        else{
+            dx=-1;
+        }
+
+
+        if(dx == dy || dx == dy*(-1)){//bishop
+            int j = m_y+dy;
+            for(int i = m_x+dx;  i !=x; i += dx){
+
+                if(g.getPiece(i,j)!=null) {
+                    return false;
+                }
+                j+=dy;       }
+
+            return true;
+
+        }
+        else //rook
+        {
+            if (m_x == x) {
+                // Horizontal
+                if (m_y < y) {
+                    // Move right
+                    for (int i = m_y + 1; i <= y; ++i)
+                    {
+
+                        if (g.getPiece(m_x, i) != null && g.getPiece(m_x, i-1) != null)
+                        {
+                            System.out.println("down");
+                            return false;
+                        }
+                    }
+                } else {
+                    // Move left
+                    for (int i = m_y - 1; i >= y; --i)
+                    {
+                        if (g.getPiece(m_x, i) != null && g.getPiece(m_x, i+1) != null)
+                        {
+                            System.out.println("up");
+                            return false;
+                        }
+                    }
+                }
+            } else if (m_y == y) {
+                // Vertical
+                if (m_x < x) {
+                    // Move down
+                    for (int i = m_x + 1; i <= x; ++i)
+                    {
+                        if (g.getPiece(i, m_y) != null && g.getPiece(i-1,m_y) != null)
+                        {
+                            System.out.println("right");
+                            return false;
+                        }
+                    }
+                } else {
+                    // Move up
+                    for (int i = m_x - 1; i >= x; --i)
+                    {
+                        if (g.getPiece(i, m_y) != null && g.getPiece(i+1,m_y) != null)
+                        {
+                            System.out.println("left");
+                            return false;
+
+                        }
+
+                    }
+                }
+            } else {
+                // Not a valid rook move (neither horizontal nor vertical)
+                return false;
+            }
+        }
+
+
+
+
+
+
+
 
         return true;
     }
